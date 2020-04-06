0ab6a32ac67abff294293ad68c63f213899dba33
Vishwaak <chandranvishwaak@gmail.com>
2020-04-06 22:48:03 +0530

    Statefull widget

--- unit_converter/lib/main.dart
+++ unit_converter/lib/main.dart
@@ -10,7 +10,15 @@ class UnitConverterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Unit _Converter',
+      title: 'Unit _Converter', 
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blueAccent[200],
+        fontFamily: 'Georgia',
+        textTheme:TextTheme(
+          headline: TextStyle(fontSize: 0.72),
+          body1: TextStyle(fontSize: 0.42))
+      ),
       home: CategoryRoute(),
     );
   }
